5b6f379281f25ead9308fdd8192474c30e8effae
MatTheoo89 <matteobellomo89@gmail.com>
2022-10-17 15:42:20 +0200

    [bacup]

---
+++ milestone-2/assets/js/script.js
@@ -0,0 +1,60 @@
+console.log('link inserito ok');
+
+const imgArray = [
+  '01.jpg',
+  '02.jpg',
+  '03.jpg',
+  '04.jpg',
+  '05.jpg'
+];
+
+const prev = document.querySelector('.up');
+const next = document.querySelector('.down');
+// console.log(next, prev);
+
+let imgTags = '';
+
+const slider = document.querySelector('.container-image');
+// console.log(slider);
+
+for(let i = 0; i < imgArray.length; i++){
+  imgTags += `
+  <img class="item hide" src="assets/img/${imgArray[i]}" ${imgArray[i]}">
+  `;
+  // console.log(imgTags);
+}
+
+let counterImg = 0;
+
+slider.innerHTML = imgTags;
+
+const items = document.getElementsByClassName('item');
+// console.log(items);
+
+items[counterImg].classList.add('visible');
+
+
+next.addEventListener('click', function(){
+
+
+});
+
+
+
+
+
+
+
+// next.addEventListener('click', function(){
+  
+//   items[counterImg].classList.remove('visible');
+//   counterImg++;
+//   items[counterImg].classList.add('visible');
+
+// });
+
+// prev.addEventListener('click', function(){
+//   items[counterImg].classList.remove('visible');
+//   counterImg--;
+//   items[counterImg].classList.add('visible');
+// });
